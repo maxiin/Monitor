fb5497a4d6e8008a3cfaab59d2f33dc74de0df95
Tiga! <maxiinne@proton.me>
2023-04-11 23:46:51 -0300

    initial, with clock & edit working

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:monitor/window.dart';
+import 'package:window_manager/window_manager.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.black,
+        fontFamily: 'NewMedia',
+      ),
+      home: Window(),
+    );
+  }
+}
---
+++ lib/widgets/screen_widget.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+abstract class ScreenWidget {
+  Widget get widget;
+  Widget get editWidget;
+}
